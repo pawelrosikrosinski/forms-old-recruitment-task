21d6a226a38d81b435f515eeada8b7be862329b3
prr <pawel.rosik.rosinski@gmail.com>
2023-05-17 11:10:16 +0200

    Bug fix and added foreign keys refs.

--- db_schema.sql
+++ db_schema.sql
@@ -2,34 +2,34 @@ CREATE ROLE admin  WITH LOGIN SUPERUSER PASSWORD 'password';
 
 create table if not exists FormTemplates();
 
-alter table FormTemplates add column if not exists id bigserial;
+alter table FormTemplates add column if not exists id bigserial primary key;
 
 alter table FormTemplates add column if not exists name varchar(30);
 
 
 create table if not exists FormTemplates_questions();
 
-alter table FormTemplates_questions  add column if not exists id bigserial;
+alter table FormTemplates_questions  add column if not exists id bigserial primary key;
 
-alter table FormTemplates_questions  add column if not exists FormTemplates_id bigint;
-
-alter table FormTemplates_questions  add column if not exists question text;
+alter table FormTemplates_questions  add column if not exists FormTemplates_id bigint references FormTemplates(id);
 
 
-create table if not exists FormTemplates_pollquestions()
 
-alter table FormTemplates_pollquestions  add column if not exists id bigserial;
 
-alter table FormTemplates_pollquestions  add column if not exists FormTemplates_id bigint;
+create table if not exists FormTemplates_pollquestions();
+
+alter table FormTemplates_pollquestions  add column if not exists id bigserial primary key;
+
+alter table FormTemplates_pollquestions  add column if not exists FormTemplates_id bigint references FormTemplates(id);
 
 alter table FormTemplates_pollquestions  add column if not exists pollquestion text;
 
 
-create table if not exists FormTemplates_pollquestions_relations()
+create table if not exists FormTemplates_pollquestions_relations();
 
-alter table FormTemplates_pollquestions_relations  add column if not exists id bigserial;
+alter table FormTemplates_pollquestions_relations  add column if not exists id bigserial primary key;
 
-alter table FormTemplates_pollquestions_relations  add column if not exists pollquestions_id bigint;
+alter table FormTemplates_pollquestions_relations  add column if not exists pollquestions_id bigint references FormTemplates_pollquestions(id);
 
 alter table FormTemplates_pollquestions_relations  add column if not exists if varchar(10);
 
