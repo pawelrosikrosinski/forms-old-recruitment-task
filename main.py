51c80dfa4887e7c0cecc00a8155c8ac34075dc77
prr <pawel.rosik.rosinski@gmail.com>
2023-05-18 02:52:52 +0200

    Basic routing added.

--- main.py
+++ main.py
@@ -3,13 +3,14 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-from flask import Flask
+from flask import Flask, request
 import psycopg2
 from psycopg2.extras import DictCursor
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     connection = psycopg2.connect("dbname=forms user=admin host=localhost password=password", cursor_factory=DictCursor)
+    connection.autocommit = True
 
     cursor = connection.cursor()
 
@@ -18,11 +19,47 @@ if __name__ == '__main__':
     app = Flask(__name__)
 
 
-    @app.route("/")
-    def hello_world():
-        return "<p>Hello, World!</p>"
+    @app.route("/get_forms_list")
+    def get_forms_list():
+        cursor.execute("execute get_forms_list")
+        return cursor.fetchall()
 
 
-    app.run()
+    @app.route("/get_form_qa")
+    def get_form_qa():
+        cursor.execute(f"execute get_form_qa({request.args.get('forms_id')})")
+        return cursor.fetchone()
+
+
+    @app.route("/get_form_poll")
+    def get_form_poll():
+        cursor.execute(f"execute get_form_qa({request.args.get('forms_id')})")
+        return cursor.fetchone()
+
+
+    @app.route("/get_searches")
+    def get_searches():
+        cursor.execute(f"execute get_searches")
+        return cursor.fetchone()
+
+
+    @app.route("/create_new_form")
+    def create_new_form():
+        cursor.execute(f"execute create_new_form ({request.args.get('formtemplates_id')})")
+        return cursor.fetchone()
+
+
+    @app.route("/post_form_qa")
+    def post_form_qa():
+        cursor.execute(f"execute post_form_qa ({request.get_json()})")
+        return cursor.fetchone()
+
+
+    @app.route("/post_form_poll")
+    def post_form_poll():
+        cursor.execute(f"execute post_form_poll ({request.get_json()})")
+        return cursor.fetchone()
+
+app.run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
